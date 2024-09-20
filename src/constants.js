91e440a615bfbab63c211446390b85913c2d06f7
Owais Mehmood <20-arid-815@student.uaar.edu.pk>
2024-09-20 16:24:29 +0500

    new commit

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // export const backendUrl = "https://usquaresolutionbackend.decimalsolution.com";
 
+export const backendUrl = "https://usquare-backend-9701358245d7.herokuapp.com";
 
 
-
-export const backendUrl ="http://localhost:8080";
+// export const backendUrl ="http://localhost:8080";
